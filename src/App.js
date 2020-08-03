79dc43da4707f2c45c6487e399ebff2177816173
homfarnam <homfarnam@gmail.com>
2020-08-03 23:51:58 +0430

    add SpotifyWebApi

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import './App.css';
-import Login from './components/login/Login';
+import React from "react";
+import "./App.css";
+import Login from "./components/login/Login";
+import { useEffect, useState } from "react";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from 'spotify-web-api-js'
+
+const spotify = new SpotifyWebApi()
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token)
+    }
+
+    console.log("I gave a token >>>", token);
+  }, []);
+
   return (
-    <div className="App">
-      <Login />
-
-
+    <div className="app">
+      {token ? (
+        // <Player />
+        <h1>I am logged in</h1>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Login.css";
+import { loginUrl } from "../../spotify";
 
 const Login = () => {
   return (
@@ -8,8 +9,8 @@ const Login = () => {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
-        
-      <a href=''>Login with spotify</a>
+
+      <a href={loginUrl}>Login with spotify</a>
     </div>
   );
 };
