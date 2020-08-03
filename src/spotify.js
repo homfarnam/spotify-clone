ee28173b87a21cf052c72dbbf537ed1e564046bc
homfarnam <homfarnam@gmail.com>
2020-08-03 23:12:12 +0430

    create login page

---
+++ src/spotify.js
@@ -0,0 +1,8 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+
+
+export const authEndpoint = 'https://accounts.spotify.com/authorize'
+
+
+const redirectUri = 'http://localhost:3000/'
+
