be7e63adc7f0cf8d8c02f7a04b194c29372586d6
homfarnam <homfarnam@gmail.com>
2020-08-04 15:02:35 +0430

    some changes in player function

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import { useEffect, useState } from "react";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from 'spotify-web-api-js'
 import Player from "./components/player/Player";
+import {useDataLayerValue} from './states/DataLayer'
 
 const spotify = new SpotifyWebApi()
 
 function App() {
-  const [token, setToken] = useState(null);
+  // const [token, setToken] = useState(null);
+  const [{user,token},dispatch] = useDataLayerValue()
+
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -19,21 +22,35 @@ function App() {
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token);
+      dispatch({
+        type:'SET_TOKEN',
+        token:_token
+      })
+
+
       spotify.setAccessToken(_token)
-      
+
       spotify.getMe().then(user=>{
-        console.log(user)
+
+        dispatch({
+          type:'SET_USER',
+          user: user
+
+        })
+
       })
     }
 
     console.log("I gave a token >>>", token);
   }, []);
 
+
+
+
   return (
     <div className="app">
       {token ? (
-        <Player />
+        <Player spotify={spotify} />
        ) : (
         <Login />
       )}
--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Player.css'
+import Sidebar from '../sidebar/Sidebar'
+import Body from '../body/Body'
+import Footer from '../footer/Footer'
 
-function Player() {
+function Player({player}) {
     return (
-        <div>
-            <h1>welcome to spotify</h1>
+        <div className='player'>
+            <div className="player__body">
+                <Sidebar />
+                <Body /> 
+            </div>
+
+            <Footer />
         </div>
     )
 }
