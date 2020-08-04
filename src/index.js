8394a9867df83e516d3f3891c312c12869c42417
homfarnam <homfarnam@gmail.com>
2020-08-04 13:39:28 +0430

    handle react context api in index

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {DataLayer} from './states/DataLayer'
+import reducer, { initialState } from './states/reducer';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DataLayer initialState={initialState}  reducer={reducer}>
+      <App />
+    </DataLayer>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
+ 
